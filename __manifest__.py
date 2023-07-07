209943aac167a47682e76bbcf98b58ce5a38da35
Sébastien Bühl <buhlsebastien@gmail.com>
2023-07-07 09:21:51 +0200

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Bonuses GSE',
+    'author': "Sébastien Bühl, Shortcut1337",
+    'website': "http://www.buhl.be",
+    'version': '0.1',
+    'category': 'Hidden',
+    'license': 'LGPL-3',
+    'summary': 'Bonuses for Technicians and Project Manager',
+    'description': """
+        Compute automatically the bonuses for the technicians and project manager 
+    """,
+    'depends': ['sale_stock', ],
+    'data': [
+            
+    ],
+    'installable': True,
+    
+}
